8ac90d5131e66750882d304139c24e55e7ff2a46
22100211ChaewonKim <1zzang@handong.ac.kr>
2022-09-06 22:28:44 +0900

    change

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        //new WordManager().start();
-        System.out.println("hello");
+        new WordManager().start();
     }
 }
